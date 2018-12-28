a53abf44e474baef02d90efa0ec1b399b21ebcf6
Adam Pisula <adam.pisula@outlook.com>
2018-12-28 15:02:29 +0100

    Logging, comments, HTTP server

--- server.js
+++ server.js
@@ -1,20 +1,65 @@
+//REQUIREs
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const connect = require('connect');
+const serveStatic = require('serve-static');
+const winston = require('winston');
 
+//CONFIG
 const config = require('./config.json');
+
+//LOGGING
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    defaultMeta: {service: 'user-service'},
+    transports: [
+        new winston.transports.File({ filename: 'log/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'log/combined.log' })
+    ]
+});
+
+if (config.environment == 'development') {
+    logger.add(new winston.transports.Console());
+}
+
+//LOGGING MIDDLEWARE
+function logMiddleware(req, res, next) {
+    logger.info({ originalUrl: req.originalUrl, method: req.method, body: req.body });
+
+    next();
+}
+
+//INITIALISE VARIABLES
 const app = express();
-const port = process.env.PORT || config.apiPort;
+const apiPort = process.env.API_PORT || config.apiPort;
+const httpPort = process.env.HTTP_PORT || config.httpPort;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logMiddleware);
 
+//CONNECT TO MONGODB
 MongoClient.connect(config.mongoAddress, (err, database) => {
-    if (err)
-        return console.log(err);
+    //COULDN'T CONNECT TO MONGODB
+    if (err) {
+        logger.error(err);
+        process.exit(1);
+    }
 
+    //GET ALL THE ROUTES FOR THE API
     require('./api/routes')(app, database);
 
-    app.listen(port, () => {
-        console.log('API on localhost:' + port);
+    //MAKE THE API LISTEN ON apiPort
+    app.listen(apiPort, () => {
+        logger.info('API on localhost:' + apiPort);
+
+        //CREATE HTTP SERVER ON httpPort -> SERVES /www
+        connect().use(serveStatic(__dirname + '/www')).listen(httpPort, function(){
+            logger.info('HTTP on localhost:' + httpPort);
+        });
     });               
 })
