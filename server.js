0b7f098e74931e99fa29812a59689df028503e5b
Adam Pisula <adam.pisula@outlook.com>
2018-12-31 01:18:56 +0100

    EJS, fully functioning webserver

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const connect = require('connect');
-const serveStatic = require('serve-static');
 const winston = require('winston');
 const ip = require('ip');
+const path = require('path');
+const requestIp = require('request-ip');
 
 //CONFIG
 const config = require('./config.json');
@@ -19,8 +19,8 @@ const logger = winston.createLogger({
     ),
     defaultMeta: {service: 'user-service'},
     transports: [
-        new winston.transports.File({ filename: 'log/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'log/combined.log' })
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' })
     ]
 });
 
@@ -36,12 +36,30 @@ function logMiddleware(req, res, next) {
 }
 
 //INITIALISE VARIABLES
-const app = express();
+const api = express();
+const webserver = express();
 const apiPort = process.env.API_PORT || config.apiPort;
 const httpPort = process.env.HTTP_PORT || config.httpPort;
+const localIp = ip.address();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(logMiddleware);
+//SET UP API AND WEBSERVER
+api.use(bodyParser.urlencoded({ extended: true }));
+api.use(logMiddleware);
+api.use(requestIp.mw());
+
+webserver.use(express.static(path.join(__dirname, 'www/views'))); 
+webserver.engine('.ejs', require('ejs').__express);
+webserver.set('views', path.join(__dirname, 'www/views'));
+webserver.set('view engine', 'ejs');
+webserver.use(requestIp.mw());
+
+webserver.get('/favicon.ico' , function(req , res) {
+    //HANDLE FAVICON
+});
+
+webserver.get('*', function(req, res) {
+    res.render(path.join(__dirname, 'www/views') + req.url, { localIp: localIp, clientIp: req.clientIp.split(':')[3] });
+});
 
 //CONNECT TO MONGODB
 MongoClient.connect(config.mongoAddress, (err, database) => {
@@ -52,15 +70,15 @@ MongoClient.connect(config.mongoAddress, (err, database) => {
     }
 
     //GET ALL THE ROUTES FOR THE API
-    require('./api/routes')(app, database);
+    require('./api/routes')(api, database);
 
     //MAKE THE API LISTEN ON apiPort
-    app.listen(apiPort, () => {
-        logger.info('API on ' + ip.address() + ':' + apiPort);
+    api.listen(apiPort, () => {
+        logger.info('API on ' + localIp + ':' + apiPort);
 
-        //CREATE HTTP SERVER ON httpPort -> SERVES /www
-        connect().use(serveStatic(__dirname + '/www')).listen(httpPort, function(){
-            logger.info('HTTP on ' + ip.address() + ':' + httpPort);
+        //CREATE WEBSERVER ON httpPort -> SERVES www/views
+        webserver.listen(httpPort, () => {
+            logger.info('HTTP on ' + localIp + ':' + httpPort);
         });
     });               
 })
