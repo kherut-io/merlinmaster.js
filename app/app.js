eb35829160b5e5e867962f1552d045e864d71749
Adam Pisula <adam.pisula@outlook.com>
2019-01-15 20:57:38 +0100

    Webserver, though authentication doesn't seem to work when you try to login from :8000

--- app/app.js
+++ app/app.js
@@ -1,28 +1,53 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const ip = require('ip');
+const cors = require('cors');
 
 const LocalStrategy = require('passport-local').Strategy;
 
 const root = path.resolve(path.dirname(require.main.filename), '..');
 
 const api = express();
+const webserver = express();
 const config = require(root + '/config')(true);
 
+const apiPort = process.env.API_PORT || config.apiPort;
+const httpPort = process.env.HTTP_PORT || config.httpPort;
+const localIP = ip.address();
+
+//SET UP API AND WEBSERVER
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: true }));
-api.use(cookieParser());
-api.use(require('express-session')({
-    secret: 'session-secret',
-    resave: false,
-    saveUninitialized: false
+api.use(cors());
+api.use(cookieSession({
+    maxAge: 24*60*60*1000,
+    keys: config._cookieKeys
 }));
 api.use(passport.initialize());
 api.use(passport.session());
+api.use(function(req, res, next) {
+    next();
+});
+
+webserver.use(express.static(path.join(root, '/app/www/views'))); 
+webserver.engine('.ejs', require('ejs').__express);
+webserver.set('views', path.join(root, '/app/www/views'));
+webserver.set('view engine', 'ejs');
+
+webserver.get('/favicon.ico', (req, res) => {
+    //HANDLE FAVICON
+});
+
+webserver.get('*', (req, res) => {
+    if(typeof req.user == 'undefined')
+        return res.render(root + '/app/www/login', { config: config, localIP: localIP });
+
+    res.render(root + '/app/www/' + req.url, { user: req.user, config: config, localIP: localIP });
+});
 
 //ROUTES
 api.use('/user', require('./api/routes/user.route'));
@@ -38,10 +63,19 @@ passport.deserializeUser(Account.deserializeUser());
 //CONNECT TO MONGODB
 mongoose.connect(config.mongo._address, { useNewUrlParser: true });
 
-api.listen(config.apiPort, (err) => {
+api.listen(apiPort, (err) => {
     if(err)
         console.log('Something didn\'t go as expected :(');
 
-    else
-        console.log('Listening on http://' + ip.address() + ':' + config.apiPort + '/');
+    else {
+        console.log('API listening on http://' + localIP + ':' + apiPort + '/');
+
+        webserver.listen(httpPort, (err) => {
+            if(err)
+                console.log('Something didn\'t go as expected :(');
+
+            else
+                console.log('HTTP listening on http://' + localIP + ':' + httpPort + '/');
+        });
+    }
 });
